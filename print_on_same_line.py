366293b018bae4f1ebc457b844beff522ed4901b
John Ashurst - MAFFAS <johnashu1@hotmail.com>
2019-07-17 08:58:53 +0200

    Update print_on_same_line.py

--- print_on_same_line.py
+++ print_on_same_line.py
@@ -2,7 +2,7 @@ from time import sleep
 import sys
 
 BLINK = 0.2
-chars_in_line = 50
+chars_in_line = 10
 
 def open_file(fn):
     with open(fn, "r") as f:
@@ -16,20 +16,17 @@ def type_out_text_to_line(text, end="", sep="", line_chars=None, blink=0.2):
 
     c = line_chars
     for i, j in enumerate(text):
-        sys.stdout.flush()
         sleep(blink)
-        print(j, end=end, sep=sep)
+        print(j, end=end, sep=sep, flush=True)
         if line_chars:
             if i + 1 == c:
-                # print()
+                print()
                 c += line_chars
     return ""
-
 
 print("\n\tSTART PRINTING\n")
 
 for text in open_file('example.txt'):
     if len(text) > 1:
-        print(type_out_text_to_line(text, line_chars=None, blink=BLINK, sep=""))
-
+        print(type_out_text_to_line(text, line_chars=chars_in_line, blink=BLINK, sep=""))
 print("\n\tEND PRINTING\n")
